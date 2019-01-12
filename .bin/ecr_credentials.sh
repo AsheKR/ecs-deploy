aa0adb5c0c532fcae84dd7fa7961dd60563c8a26
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-11 23:59:39 -0800

    add ecr push

--- .bin/ecr_credentials.sh
+++ .bin/ecr_credentials.sh
@@ -1,1 +1,15 @@
-#!/usr/bin/env bash
+#! /bin/bash
+
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials << EOL
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+EOL
+
+cat > ~/.aws/config << EOL
+[default]
+region = ap-northeast-2
+output = json
+EOL
