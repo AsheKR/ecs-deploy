03ef72ad6c688f06b02e81a00b4dd67954e5ae08
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-11 22:08:42 -0800

    add amazon allowed_hosts

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,3 +1,5 @@
+import requests
+
 from .base import *
 
 PRODUCTION_JSON = json.load(open(os.path.join(SECRET_DIR, 'production.json')))
@@ -5,9 +7,17 @@ PRODUCTION_JSON = json.load(open(os.path.join(SECRET_DIR, 'production.json')))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+# 아마존에서 제공해주는 URL에 접속을 허용하는 코드
 ALLOWED_HOSTS = [
-
+    '.amazonaws.com',
 ]
+
+# Health Check 도메인을 허용하는 코드
+try:
+    EC2_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4').text
+    ALLOWED_HOSTS.append(EC2_IP)
+except requests.exceptions.RequestException:
+    pass
 
 STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
 
