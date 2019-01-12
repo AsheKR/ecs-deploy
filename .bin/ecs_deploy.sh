e25762efffa071a14516015617c080eb84164f2e
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-12 00:46:54 -0800

    add ecs_deploy

--- .bin/ecs_deploy.sh
+++ .bin/ecs_deploy.sh
@@ -1,1 +1,13 @@
 #! /bin/bash
+# Deploy only if it's not a pull request
+if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+# Deploy only if we're testing the master branch
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+echo "Deploying $TRAVIS_BRANCH on $TASK_DEFINITION"
+ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $REMOTE_IMAGE_URL:latest
+else
+echo "Skipping deploy because it's not an allowed branch"
+fi
+else
+echo "Skipping deploy because it's a PR"
+fi
